217b3df92c49e346860ea8b4436b7bb65a4da4d7
JonahCoffelt <coffelt.jonah0918@gmail.com>
2025-02-07 20:20:25 -0600

    game framework

--- game.py
+++ game.py
@@ -1,10 +1,35 @@
 import basilisk as bsk
 import glm
 
-engine = bsk.Engine()
-scene = bsk.Scene()
-engine.scene = scene
 
-while engine.running:
-    
-    engine.update()
+class Game:
+    def __init__(self) -> None:
+        self.engine = bsk.Engine()
+        self.scene = bsk.Scene()
+        self.engine.scene = self.scene
+
+    def load_shaders(self) -> None:
+        self.plain_shader = bsk.Shader(self.engine, vert="shaders/plain.vert", frag="shaders/plain.frag")
+        self.engine.shader = self.plain_shader
+
+    def load_level(self) -> None:
+        """
+        Add all nodes to the scene
+        """
+
+        self.scene.add(bsk.Node())
+
+    def start(self) -> None:
+        """
+        Starts the engine and the game
+        """
+        
+        self.load_shaders()
+        self.load_level()
+
+        while self.engine.running:
+            self.engine.update()
+
+
+game = Game()
+game.start()
