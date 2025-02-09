64cb81dff1517977e6f7273e92d97ee61276e99f
Isaac_Lagoy <isaacblagoy@gmail.com>
2025-02-08 22:11:20 -0600

    Added level queuing

--- game.py
+++ game.py
@@ -7,6 +7,7 @@ from weapons.gun import Gun
 from weapons.bullet import Bullet
 from scenes.game_scene import GameScene, get_plain_nodes
 from scenes.test_scene import test_scene
+from scenes.levels import level1
 from render.render_handler import RenderHandler
 from ui.ui import UI
 # import cudart
@@ -33,6 +34,8 @@ class Game():
         self.particle_shader = bsk.Shader(self.engine, 'shaders/particle_sight.vert', 'shaders/particle_sight.frag')
         self.invisible_shader = bsk.Shader(self.engine, 'shaders/invisible.vert', 'shaders/invisible.frag')
         self.sight_scene.particle = bsk.ParticleHandler(self.sight_scene, self.particle_shader)
+        
+        self.levels = [level1]
         
         self.pistol = Gun(
             game = self,
@@ -146,6 +149,10 @@ class Game():
         self.green = bsk.Material(color=(saturation, 255, saturation), roughness=.8, metallicness=0.0, specular=0.25)
         self.blue = bsk.Material(color=(saturation, saturation, 255), roughness=.8, metallicness=0.0, specular=0.25)
         self.white = bsk.Material(color=(255, 255, 255))
+        
+    def next_level(self):
+        level = self.levels.pop(0)
+        return level
 
     def start(self) -> None:
         """
@@ -155,13 +162,13 @@ class Game():
         self.render_handler = RenderHandler(self)
         self.load_meshes()
         
-        self.load_level(test_scene(self))
+        self.load_level(self.next_level()(self))
 
         while self.engine.running:
 
             if self.engine.keys[bsk.pg.K_1] and not self.engine.previous_keys[bsk.pg.K_1]:
                 self.ui.add_transition()
-                self.ui.add_transition(duration=1.5, callback= lambda: self.load_level(test_scene(self)))
+                self.ui.add_transition(duration=1.5, callback= lambda: self.load_level(self.next_level()(self)))
 
             self.bullet_handler.update(self.engine.delta_time)
             self.enemy_handler.update(self.engine.delta_time)
