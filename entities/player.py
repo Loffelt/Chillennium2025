7e6ff70ebfa7aec1091cb141b4c7b810c5bb58b2
Isaac_Lagoy <isaacblagoy@gmail.com>
2025-02-08 23:10:47 -0600

    enter

--- entities/player.py
+++ entities/player.py
@@ -82,6 +82,7 @@ class Player(Entity):
         
         self.game.bullet_handler.bullets += bullets
         
+        
     @property
     def position(self): return self._position
     @property
